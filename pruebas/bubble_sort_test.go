93db6638a603f8919eacd8ed05bff2719d1594d5
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-08 21:45:54 -0500

    Pruebita simple

---
+++ pruebas/bubble_sort_test.go
@@ -0,0 +1,19 @@
+package pruebas
+
+import (
+	"analisis-algoritmos/ordenamiento"
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T){
+  esperado:=[]int {0,1,2,3,4}
+  resultado:=[]int{4,2,1,3,0}
+
+
+  ordenamiento.BubbleSort(&resultado)
+
+  if !reflect.DeepEqual(resultado,esperado){
+    t.Fail()
+  }
+}
