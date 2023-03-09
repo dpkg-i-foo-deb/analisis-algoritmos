f87d4d06f907980af9ac0a2432ffeb21c3d402c1
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-09 18:50:17 -0500

    Strand sort

---
+++ ordenamiento/strand_sort.go
@@ -0,0 +1,36 @@
+package ordenamiento
+
+func StrandSort(arr *[]int) []int {
+	if len(*arr) <= 1 {
+		return *arr
+	}
+
+	// Split the array into sorted and unsorted parts
+	sorted := []int{(*arr)[0]}
+	*arr = (*arr)[1:]
+
+	for len(*arr) > 0 {
+		// Split the remaining array into subarrays that are sorted
+		sub := []int{(*arr)[0]}
+		*arr = (*arr)[1:]
+
+		for i := 0; i < len(sorted); i++ {
+			if sorted[i] < sub[0] {
+				continue
+			}
+			j := i
+			for j < len(sorted) && sub[len(sub)-1] > sorted[j] {
+				sub = append(sub, sorted[j])
+				j++
+			}
+			sorted = append(sorted[:i], append(sub, sorted[i:]...)...)
+			break
+		}
+
+		if len(sorted) == 0 || sub[len(sub)-1] > sorted[len(sorted)-1] {
+			sorted = append(sorted, sub...)
+		}
+	}
+
+	return sorted
+}
