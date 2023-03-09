38d681209702b8c06ae7fb3894b5a8ac0848ceab
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-09 13:18:52 -0500

    Medir tiempo en la prueba

--- pruebas/bubble_sort_test.go
+++ pruebas/bubble_sort_test.go
@@ -2,6 +2,7 @@ package pruebas
 
 import (
 	"analisis-algoritmos/ordenamiento"
+	"analisis-algoritmos/tiempo"
 	"reflect"
 	"testing"
 )
@@ -10,6 +11,7 @@ func TestBubbleSort(t *testing.T) {
 	esperado := []int{0, 1, 2, 3, 4}
 	resultado := []int{4, 2, 1, 3, 0}
 
+  defer tiempo.MedirTiempo("Bubble sort")()
 	ordenamiento.BubbleSort(&resultado)
 
 	if !reflect.DeepEqual(resultado, esperado) {
