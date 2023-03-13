47ac672b22ed8f2c3678930890b6b0a96e332a15
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-13 08:31:51 -0500

    Generar arreglo de 50 mil elementos

--- cmd/crear_arreglos.go
+++ cmd/crear_arreglos.go
@@ -10,10 +10,12 @@ import (
 )
 
 var archivo10Mil *os.File
+var archivo50Mil *os.File
 var archivo100Mil *os.File
 var archivo1Millon *os.File
 
 var arreglo10Mil modelos.Arreglo
+var arreglo50Mil modelos.Arreglo
 var arreglo100Mil modelos.Arreglo
 var arreglo1Millon modelos.Arreglo
 
@@ -32,20 +34,25 @@ func init() {
 
 func crearArreglos(cmd *cobra.Command, args []string) {
 	archivo10Mil = util.CrearArchivo("arreglo-10-mil.json")
+	archivo50Mil = util.CrearArchivo("arreglo-50-mil.json")
 	archivo100Mil = util.CrearArchivo("arreglo-100-mil.json")
 	archivo1Millon = util.CrearArchivo("arreglo-1-millon.json")
 
 	arreglo10Mil.Arr = util.GenerarArreglo(10000)
+
+	arreglo50Mil.Arr = util.GenerarArreglo(50000)
 
 	arreglo100Mil.Arr = util.GenerarArreglo(100000)
 
 	arreglo1Millon.Arr = util.GenerarArreglo(1000000)
 
 	encoder10Mil := json.NewEncoder(archivo10Mil)
+	encoder50Mil := json.NewEncoder(archivo50Mil)
 	encoder100Mil := json.NewEncoder(archivo100Mil)
 	encoder1Millon := json.NewEncoder(archivo1Millon)
 
 	encoder10Mil.Encode(arreglo10Mil)
+	encoder50Mil.Encode(arreglo50Mil)
 	encoder100Mil.Encode(arreglo100Mil)
 	encoder1Millon.Encode(arreglo1Millon)
 }
